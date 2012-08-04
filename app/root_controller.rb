3430dbd8f07dcbf1a13b1ac63b0076768d905ce4
Gant <GantMan@gmail.com>
2012-08-04 17:00:32 -0500

    figure it out

--- app/root_controller.rb
+++ app/root_controller.rb
@@ -31,6 +31,10 @@ class RootController < UIViewController
     #CGContextScaleCTM(currentContext, 1, -1)
 
     scale = UIScreen.mainScreen.scale
+
+    #if (scale > 1.0)
+    #CGContextScaleCTM(currentContext, 0.5, 0.5)
+    #end
     
     features.each_with_index do |feature, index|
       
@@ -47,52 +51,33 @@ class RootController < UIViewController
       p "Left Eye Position = #{feature.leftEyePosition.x}" if feature.hasLeftEyePosition
       p "Right Eye Position = #{feature.rightEyePosition.x}" if feature.hasRightEyePosition
       p "Mouth Position = #{feature.mouthPosition.x}" if feature.hasMouthPosition
+      
+      if feature.hasRightEyePosition
+        drawFeatureInContext(currentContext, atPoint:feature.leftEyePosition)
+      end
     end
 
     newView = UIImageView.alloc.initWithFrame(view.bounds)
-    #newView.contentMode = UIViewContentModeScaleAspectFit
     newView.image = UIGraphicsGetImageFromCurrentImageContext()
+    
+    newView = UIImageView.alloc.initWithImage(UIGraphicsGetImageFromCurrentImageContext())
+    newView.frame = CGRectMake(0, 0, 200, 200)
+    
     UIGraphicsEndImageContext()
+
 
     view.addSubview(newView)
   end
 
-  def just_rects
-
-    UIGraphicsBeginImageContextWithOptions(@me.size, true, 0)
-    @me.drawInRect(view.bounds)
-
-    path = CGPathCreateMutable()
-
-    rectangle1 = CGRectMake(10, 10, 200, 300)
-
-    rectangle2 = CGRectMake(40, 100, 90, 300)
-
-    # Handling the madness of making an array of pointers
-    # VERY Procedural, you'll want to clean this up in any ACTUAL practice
-    rects_ptr = Pointer.new(CGRect.type, 2) 
-    rects_ptr[0] = rectangle1
-    rects_ptr[1] = rectangle2
-
-    CGPathAddRects(path, nil, rects_ptr, 2)
-
-    currentContext = UIGraphicsGetCurrentContext()
-
-    CGContextAddPath(currentContext, path)
-
-    UIColor.colorWithRed(0.2, green:0.6, blue:0.8, alpha:1).setFill
-
-    UIColor.blackColor.setStroke
-
-    CGContextSetLineWidth(currentContext, 5)
-
-    CGContextDrawPath(currentContext, KCGPathFillStroke)
-
-    newView = UIImageView.alloc.initWithFrame(view.bounds)
-    #newView.contentMode = UIViewContentModeScaleAspectFit
-    newView.image = UIGraphicsGetImageFromCurrentImageContext()
-    UIGraphicsEndImageContext()
-
-    view.addSubview(newView)
+  def drawFeatureInContext(context, atPoint:featurePoint)
+    p "Drawing circle"
+    radius = 1
+    p featurePoint.y
+    p featurePoint.y + radius
+    #CGContextAddArc(context, featurePoint.x, featurePoint.y, radius, 0, 3.14 * 2, 1)
+    CGContextAddRect(context, [[featurePoint.x, featurePoint.x + 1],[featurePoint.y, featurePoint.y + radius]])
+    #CGContextAddRect(context, [[5, 6],[100, 110]])
+    CGContextDrawPath(context, KCGPathFillStroke)
   end
+
 end
